8eb2267784544513f357ebb3b8d2bb5d30ee736f
Ganesh Lekurwale <69107680+Ganeshlekurwale@users.noreply.github.com>
2024-09-01 00:11:13 +0530

    env changes

--- Backend/server.js
+++ Backend/server.js
@@ -11,7 +11,13 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
-app.use(cors());
+app.use(cors(
+  {
+    origin: [""],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+  }
+));
 
 
 app.use('/passwords', passwordRoutes);
